ca879e410bee9a074989936fc5441b8385cb6f9e
Jonathan Norman <jnorman2123@gmail.com>
2019-10-09 22:05:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,10 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" unless age > 12
+    if age >= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      nil 
+    end 
   end
 end
