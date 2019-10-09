de75f227ead59f2a68de9ba7893bcc7f406f1fc6
Jonathan Norman <jnorman2123@gmail.com>
2019-10-09 22:08:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,5 +13,5 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!" unless age > 12
-  end   
+  end
 end 
